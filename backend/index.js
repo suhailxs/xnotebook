63524c76b2600009a68198dd093377b04bd4497e
Mohd Suhail <codewithxs@gmail.com>
2022-04-16 00:02:37 +0530

    create model and routes

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,13 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+//available routes
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
+app.use('/api/auth',require('./routes/auth'));
+// app.use('/api/notes',require('./routes/notes'));
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
