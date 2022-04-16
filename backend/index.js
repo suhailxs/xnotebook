b174638f46a8193e654cf2df5cffa9a83fef0f40
Mohd Suhail <codewithxs@gmail.com>
2022-04-17 00:17:31 +0530

    fetching and adding notes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = 5000;
 app.use(express.json());
 
 app.use('/api/auth',require('./routes/auth'));
-// app.use('/api/notes',require('./routes/notes'));
+app.use('/api/notes',require('./routes/notes'));
 
 
 
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,10 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-
-router.get('/',(req ,res)=>{
-    
+//ROUTE 1 :Get all  the notes :GET  "/api/auth/getuser". require Auth
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    //catch errors
+    console.error(error.message);
+    res.status(500).send("some error occurred");
+  }
 });
 
+//ROUTE 2 :Add a new Note :POST  "/api/auth/addnote". require Auth
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 5 }),
+    body("description", "Description must be atleast 5 Character").isLength({min: 5})
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //if there are error , return bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const saveNote = await note.save();
+      res.json(saveNote);
+    } catch (error) {
+      //catch errors
+      console.error(error.message);
+      res.status(500).send("some error occurred");
+    }
+  }
+);
 
 module.exports = router;
