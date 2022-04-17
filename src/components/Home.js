bb2c37394a9c91a39fef8fa7c09c900680d47a4f
Mohd Suhail <codewithxs@gmail.com>
2022-04-18 03:20:28 +0530

    fetching notes from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+import NoteContext from "../context/notes/noteContext";
 
 export const Home = () => {
+  const context = useContext(NoteContext);
+  const {notes , setNotes} = context;
+
   return (
     <div>
       <div className="container my-3">
@@ -43,6 +47,9 @@ export const Home = () => {
       </div>
       <div className="container my-3">
       <h2>Add a note</h2>
+      {notes.map((note)=>{
+        return note.title
+      })}
       </div>
     </div>
   );
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,9 +3,29 @@ import { useState } from "react";
 
 
 const NoteState = (props)=>{
-    
+    const notesInitial = [
+        {
+          "_id": "625b149fd620279102613a3d",
+          "user": "6259e8a061e9d66fea25c091",
+          "title": "mytitle",
+          "description": "suhail wait up  haha",
+          "tag": "suhail",
+          "date": "2022-04-16T19:10:23.964Z",
+          "__v": 0
+        },
+        {
+          "_id": "625c7e8c480f27b781aa198a",
+          "user": "6259e8a061e9d66fea25c091",
+          "title": "workign on some projects",
+          "description": "Once upon a time there was a person named xstechie has a instagram page called xstechie",
+          "tag": "xstechie , suhail",
+          "date": "2022-04-17T20:54:36.141Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitial)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes , setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
